8a734efbbd7406cf3d99947e5338602f98ac9eda
Harun Bülbül <95485758+HarunBulbull@users.noreply.github.com>
2025-01-21 17:45:19 +0300

    fix and license

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const axios = require('axios');
-
 async function getBankData(id) {
   try {
-    const response = await axios.get(`https://bankapi.harunbulbul.com/${id}`);
-    console.log(response.data);
+    const response = await fetch(`https://bankapi.harunbulbul.com/${id}`, {method: "GET",});
+    const data = await response.json();
+    if(response.ok){return {succes:true, ...data}}
+    else{return {succes:false, error: data.error}}
   } catch (error) {
     console.error(error);
   }
